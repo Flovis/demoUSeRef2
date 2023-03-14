647496d557d8a5c5f9c6155187929c085988f969
Flovis <floviskabandafkm@gmail.com>
2023-03-14 12:22:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./style/app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const input = useRef();
+    const titre = useRef();
+
+    const clicked = () => {
+        // input.current.value = 'Bonjour';
+        titre.current.innerText = input.current.value;
+    };
+
+    return (
+        <div>
+            <h1 ref={titre}></h1>
+            <input type="text" name="" id="" ref={input} />
+            <button onClick={clicked}>Clicked</button>
+        </div>
+    );
+};
 
 export default App;
